8c1ed9583886a70e2b3ad4bfd213e1f756710492
Peter <mail@ptrwlsr.com>
2021-06-24 21:24:03 -0500

    changed to bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const {registerValidation, loginValidation} = require('../utils/validation');
 
